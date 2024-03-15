e511e5d01289a3ee372026b91c50336772b5393f
1raghav-bhatia <1raghavbhatia@gmail.com>
2024-03-15 11:43:55 -0400

    This pushes the complete R script for downloading and storing the raw datasets.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,47 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the ces20 dataset from the Harvard dataverse
+# database with the required variables for the study.
+# Author: Carl Fernandes, Lexi Knight and Raghav Bhatia
+# Date: 12 March 2024
+# Contact: raghav.bhatia@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dataverse)
+library(gutenbergr)
+library(janitor)
+library(knitr)
+library(readr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+## Downloading the demographic variable data
 
+ces2020_demographic <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, CC20_410, gender, educ, race, marstat, region, faminc_new)
+
+## Downloading the economic variable data
+
+ces2020_economic <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, CC20_410, CC20_302, CC20_303)
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+write_csv(ces2020_demographic, "data/raw_data/ces2020_demographic_data.csv")
+write_csv(ces2020_economic, "data/raw_data/ces2020_economic_data.csv")
 
          
